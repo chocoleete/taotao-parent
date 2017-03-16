fef15c312ee3d208fe9e45e6b2f975acf585f058
userlee <gongli.li@bestlink.com.cn>
2017-03-16 10:43:44 +0800

    新增redis缓存，并整合了solr框架

--- taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -1,13 +1,18 @@
 package com.taotao.content.service.impl;
 
 import com.taotao.commom.pojo.TaotaoResult;
+import com.taotao.commom.utils.JsonUtils;
+import com.taotao.content.jedis.JedisClient;
 import com.taotao.content.service.ContentService;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
 import com.taotao.pojo.TbContentExample;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
 
@@ -20,6 +25,14 @@ public class ContentServiceImpl implements ContentService {
     //注入contentMapper
     @Autowired
     private TbContentMapper contentMapper;
+
+    //注入jedisClient
+    @Resource(name = "jedisClientPool")
+    private JedisClient jedisClient;
+
+    //取resources中的值
+    @Value(value = "${CONTENT_KEY}")
+    private String CONTENT_KEY;
 
     /**
      * 参数列表：TbContent content
@@ -35,6 +48,10 @@ public class ContentServiceImpl implements ContentService {
         content.setUpdated(date);
         //插入数据
         contentMapper.insert(content);
+
+        //缓存同步，清除redis中categoryId对应的缓存信息
+        jedisClient.hdel(CONTENT_KEY, content.getCategoryId().toString());
+
         //返回结果，然后去applicationContext-service.xml发布服务
         return TaotaoResult.ok();
     }
@@ -47,11 +64,32 @@ public class ContentServiceImpl implements ContentService {
      */
     @Override
     public List<TbContent> getContentList(long categoryId) {
+        //先查询缓存
+        try {
+            String json = jedisClient.hget(CONTENT_KEY, categoryId + "");
+            //判断是否命中缓存
+            if (StringUtils.isNotBlank(json)) {
+                //将json转换成list
+                List<TbContent> list = JsonUtils.jsonToList(json, TbContent.class);
+                return list;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         //根据categoryId查询数据库
         TbContentExample example = new TbContentExample();
         TbContentExample.Criteria criteria = example.createCriteria();
         criteria.andCategoryIdEqualTo(categoryId);
         List<TbContent> list = contentMapper.selectByExample(example);
+
+        //向缓存中保存结果
+        try {
+            jedisClient.hset(CONTENT_KEY, categoryId + "", JsonUtils.objectToJson(list));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         //返回结果，然后去aplicationContext-service.xml发布服务
         return list;
     }
